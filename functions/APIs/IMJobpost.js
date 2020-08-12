c5ad8eced81b4eec4dc9423e90ee020716669882
vamsigoteti7 <63846292+vamsigoteti7@users.noreply.github.com>
2020-08-12 17:59:40 +0000

    function added

--- functions/APIs/IMJobpost.js
+++ functions/APIs/IMJobpost.js
@@ -39,21 +39,38 @@ exports.getAlljobposts = (request, response) => {
 
 exports.getJobPostById = (request, response) => {
      db
-          .doc(`/IMjobpost/${request.params.jobpostId}`)
+          .collection('IMjobpost')
+          .doc(request.params.docid)
           .get()
           .then((doc) => {
-               if (!doc.exists) {
-                    return response.status(404).json(
-                         {
-                              error: 'Job Post not found'
-                         });
-               }
-               JobPostData = doc.data();
-               JobPostData.jobPostId = doc.id;
-               return response.json(JobPostData);
+               // let jp = [];
+
+               // jp.push({
+               //      jobpostid: doc.id,
+               //      companyname: doc.data().companyname,
+               //      companylogo: doc.data().companylogo,
+               //      experience: doc.data().experience,
+               //      gender: doc.data().gender,
+               //      jobid: doc.data().jobid,
+               //      jobposteddate: doc.data().jobposteddate,
+               //      jobpostexpires: doc.data().jobpostexpires,
+               //      place: doc.data().place,
+               //      jobdescription: doc.data().jobdescription,
+               //      jobtype: doc.data().jobtype,
+               //      vacancies: doc.data().vacancies,
+               //      username: doc.data().username,
+               //      recruiteremail: doc.data().recruiteremail,
+               //      recruiterphonenumber: doc.data().recruiterphonenumber,
+               //      recruiterlinkdinlink: doc.data().recruiterlinkdinlink,
+               //      recruiterwebsite: doc.data().recruiterwebsite,
+               //      salary: doc.data().salary,
+               //      tagline: doc.data().tagline
+               // });
+
+               return response.json({ "status": "sucess" });
           })
           .catch((err) => {
                console.error(err);
-               return response.status(500).json({ error: error.code });
+               return response.status(500).json({ error: err.code });
           });
 };
