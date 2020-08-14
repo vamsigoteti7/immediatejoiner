38e76a8a82a82ab24c157f634afe6bf8b222a89b
vamsigoteti7 <63846292+vamsigoteti7@users.noreply.github.com>
2020-08-14 08:33:17 +0000

    function added

---
+++ functions/APIs/IMIndustries.js
@@ -0,0 +1,20 @@
+exports.postindustries = (request, response) => {
+
+    const newindustryItem = {
+        industryid: request.body.industryid,
+        industryname: request.body.industryname
+    }
+
+    db
+        .collection('IMIndustries')
+        .add(newindustryItem)
+        .then((doc) => {
+            const responseindunstryItem = newindunstryItem;
+            responseindustryItem.id = doc.id;
+            return response.json(responseindustryItem);
+        })
+        .catch((error) => {
+            console.error(error);
+            response.status(500).json({ error: 'Something went wrong' });
+        });
+};
