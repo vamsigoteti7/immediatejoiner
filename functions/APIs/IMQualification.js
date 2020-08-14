f26640cfabda7d2753c04747f6bf0f78b978708a
krishnavamsi <krishnavamsideepthi@gmail.com>
2020-08-14 18:52:09 +0530

    function added

--- functions/APIs/IMQualification.js
+++ functions/APIs/IMQualification.js
@@ -20,3 +20,25 @@ exports.postQualification = (request, response) => {
             response.status(500).json({ error: 'Something went wrong' });
         });
 };
+
+exports.getAllQualifications = (request, response) => {
+    db
+        .collection('IMQualification')
+        .orderBy('qualificationid', 'desc')
+        .get()
+        .then((data) => {
+            let qualf = [];
+            data.forEach((doc) => {
+                qualf.push({
+                    qualificationdocid: doc.id,
+                    qualificationid: doc.data().qualificationid,
+                    qualificationname: doc.data().qualificationname
+                });
+            });
+            return response.json(qualf);
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({ error: err.code });
+        });
+};
