567b191bf403d9119e607f0ae85ea1f38a70d144
krishnavamsi <krishnavamsideepthi@gmail.com>
2020-08-15 00:01:33 +0530

    updated

--- functions/APIs/IMRegister.js
+++ functions/APIs/IMRegister.js
@@ -39,44 +39,43 @@ exports.uploadProfilePhoto = (request, response) => {
 	let imageToBeUploaded = {};
 	
 	busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-		// if (mimetype !== 'image/png' && mimetype !== 'image/jpeg') {
-		// 	return response.status(400).json({ error: 'Wrong file type submited' });
-		// }
-		// const imageExtension = filename.split('.')[filename.split('.').length - 1];
-        // imageFileName = `${request.user.username}.${imageExtension}`;
-		// const filePath = path.join(os.tmpdir(), imageFileName);
-		// imageToBeUploaded = { filePath, mimetype };
-		// file.pipe(fs.createWriteStream(filePath));
-		return response.json({ "mimetype": mimetype });
+		if (mimetype !== 'image/png' && mimetype !== 'image/jpeg') {
+			return response.status(400).json({ error: 'Wrong file type submited' });
+		}
+		const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        imageFileName = `${request.user.username}.${imageExtension}`;
+		const filePath = path.join(os.tmpdir(), imageFileName);
+		imageToBeUploaded = { filePath, mimetype };
+		file.pipe(fs.createWriteStream(filePath));
     });
     // // deleteImage(imageFileName);
-	// busboy.on('finish', () => {
-	// 	admin
-	// 		.storage()
-	// 		.bucket()
-	// 		.upload(imageToBeUploaded.filePath, {
-	// 			resumable: false,
-	// 			metadata: {
-	// 				metadata: {
-	// 					contentType: imageToBeUploaded.mimetype
-	// 				}
-	// 			}
-	// 		})
-	// 		.then(() => {
-	// 			//const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-	// 			// return db.doc(`/users/${request.user.username}`).update({
-	// 			// 	imageUrl
-	// 			// });
-	// 		})
-	// 		.then(() => {
-	// 			return response.json({ message: 'Image uploaded successfully' });
-	// 		})
-	// 		.catch((error) => {
-	// 			console.error(error);
-	// 			return response.status(500).json({ error: error.code });
-	// 		});
-	// });
-	// busboy.end(request.rawBody);
+	 busboy.on('finish', () => {
+		admin
+			.storage()
+			.bucket()
+			.upload(imageToBeUploaded.filePath, {
+				resumable: false,
+				metadata: {
+					metadata: {
+						contentType: imageToBeUploaded.mimetype
+					}
+				}
+			})
+			.then(() => {
+				//const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+				// return db.doc(`/users/${request.user.username}`).update({
+				// 	imageUrl
+				// });
+			})
+			.then(() => {
+				return response.json({ message: 'Image uploaded successfully' });
+			})
+			.catch((error) => {
+				console.error(error);
+				return response.status(500).json({ error: error.code });
+			});
+	 });
+	 busboy.end(request.rawBody);
 };
 
 exports.postRegister = (request, response) => {
