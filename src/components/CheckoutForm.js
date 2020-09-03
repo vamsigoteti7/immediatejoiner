7ee2a61dd432466003e48f9b655dc917fbd2b27f
vamsigoteti7 <63846292+vamsigoteti7@users.noreply.github.com>
2020-09-03 17:28:18 +0000

    function updated

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -2,8 +2,69 @@ import React, { useEffect, useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import "./CheckoutForm.css";
 import api from './api';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import StarIcon from '@material-ui/icons/StarBorder';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  },
+}));
 
 export default function CheckoutForm() {
+  const classes = useStyles();
   const [amount, setAmount] = useState(0);
   const [currency, setCurrency] = useState("");
   const [clientSecret, setClientSecret] = useState(null);
@@ -92,45 +153,125 @@ export default function CheckoutForm() {
       },
     };
 
+    
+
+    const tiers = [
+      {
+        title: 'Free',
+        price: '0',
+        description: ['10 users included', '2 GB of storage', 'Help center access', 'Email support'],
+        buttonText: 'Sign up for free',
+        buttonVariant: 'outlined',
+      },
+      {
+        title: 'Pro',
+        subheader: 'Most popular',
+        price: '15',
+        description: [
+          '20 users included',
+          '10 GB of storage',
+          'Help center access',
+          'Priority email support',
+        ],
+        buttonText: 'Get started',
+        buttonVariant: 'contained',
+      },
+      {
+        title: 'Enterprise',
+        price: '30',
+        description: [
+          '50 users included',
+          '30 GB of storage',
+          'Help center access',
+          'Phone & email support',
+        ],
+        buttonText: 'Contact us',
+        buttonVariant: 'outlined',
+      },
+    ];
+
     return (
-      <form onSubmit={handleSubmit}>
-        <h1>
-          {currency.toLocaleUpperCase()}{" "}
-          {amount.toLocaleString(navigator.language, {
-            minimumFractionDigits: 2,
-          })}{" "}
-        </h1>
-        <h4>Pay INR 100  (Validity 30 days)</h4>
+      <React.Fragment>
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+            {tiers.map((tier) => (
+              // Enterprise card is full width at sm breakpoint
+              <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+                <Card>
+                  <CardHeader
+                    title={tier.title}
+                    subheader={tier.subheader}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    action={tier.title === 'Pro' ? <StarIcon /> : null}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography component="h2" variant="h3" color="textPrimary">
+                        ${tier.price}
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
+                        /mo
+                    </Typography>
+                    </div>
+                    <ul>
+                      {tier.description.map((line) => (
+                        <Typography component="li" variant="subtitle1" align="center" key={line}>
+                          {line}
+                        </Typography>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button fullWidth variant={tier.buttonVariant} color="primary">
+                      {tier.buttonText}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <form onSubmit={handleSubmit}>
+          <h1>
+            {currency.toLocaleUpperCase()}{" "}
+            {amount.toLocaleString(navigator.language, {
+              minimumFractionDigits: 2,
+            })}{" "}
+          </h1>
+          <h4>Pay INR 100  (Validity 30 days)</h4>
 
-        <div className="sr-combo-inputs">
-          <div className="sr-combo-inputs-row">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Name"
-              autoComplete="cardholder"
-              className="sr-input"
-            />
+          <div className="sr-combo-inputs">
+            <div className="sr-combo-inputs-row">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                autoComplete="cardholder"
+                className="sr-input"
+              />
+            </div>
+
+            <div className="sr-combo-inputs-row">
+              <CardElement
+                className="sr-input sr-card-element"
+                options={options}
+              />
+            </div>
           </div>
 
-          <div className="sr-combo-inputs-row">
-            <CardElement
-              className="sr-input sr-card-element"
-              options={options}
-            />
-          </div>
-        </div>
+          {error && <div className="message sr-field-error">{error}</div>}
 
-        {error && <div className="message sr-field-error">{error}</div>}
-
-        <button
-          className="btn"
-          disabled={processing || !clientSecret || !stripe}
-        >
-          {processing ? "Processing…" : "Pay"}
-        </button>
-      </form>
+          <button
+            className="btn"
+            disabled={processing || !clientSecret || !stripe}
+          >
+            {processing ? "Processing…" : "Pay"}
+          </button>
+        </form>
+      </React.Fragment>
     );
   };
 
