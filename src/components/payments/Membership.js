a9c1a28b9a42e789dcc7a1eec34a97609c85d8d9
vamsigoteti7 <63846292+vamsigoteti7@users.noreply.github.com>
2020-09-20 07:01:33 +0000

    function added

--- src/components/payments/Membership.js
+++ src/components/payments/Membership.js
@@ -4,6 +4,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { CardElement, Elements, ElementsConsumer } from '@stripe/react-stripe-js';
 import { Link } from 'react-router-dom';
 import hero_1 from '../../images/hero_1.jpg';
+import { auth } from '../../firebase/index';
 
 class MembershipCheckout extends React.Component {
 
@@ -11,7 +12,7 @@ class MembershipCheckout extends React.Component {
         super(props);
         this.state = {
             cardholdername: '',
-            error: [],
+            error: '',
             status: '',
             plan: this.props.plan,
             stripetransactions: [],
@@ -95,9 +96,20 @@ class MembershipCheckout extends React.Component {
                     this.setState({ error: payload.error });
                     console.log("[error]", payload.error);
                 } else {
+                    firebase.firestore()
+                    .collection('IMUserPayments').add({
+                        userid: this.props.userid.user.uid,
+                        payment: payload.paymentIntent,
+                        createdDate: firebase.firestore.Timestamp.fromDate(new Date())
+                    });
+                    this.props.history.push('/RecruiterDashboard');
                     console.log("[PaymentIntent]", payload.paymentIntent);
                 }
             } catch (error) {
+
+                
+
+
                 console.log(error);
             }
         }
@@ -130,6 +142,14 @@ class MembershipCheckout extends React.Component {
                                     <li><Link to="/Contactus">Contact</Link></li>
                                 </ul>
                             </nav>
+                            <div className="right-cta-menu text-right d-flex aligin-items-center col-6">
+                                <div className="ml-auto">
+                                    <button onClick={() => { auth.signOut() }} className="btn btn-primary border-width-2 d-none d-lg-inline-block"><span className="mr-2 icon-lock_outline"></span>Log Out</button>
+                                </div>
+                                <div to="/" className="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3">
+                                    <span className="icon-menu h3 m-0 p-0 mt-2"></span>
+                                </div>
+                            </div>
                             {/* <SmartToaster
                                 store={toast}
                                 lightBackground={true}
