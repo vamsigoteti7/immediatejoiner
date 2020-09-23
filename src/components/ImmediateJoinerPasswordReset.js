b9b04ef85f0c2204b5f4a825845d9394d2e6e02c
krishnavamsi <krishnavamsideepthi@gmail.com>
2020-09-23 16:30:52 +0530

    function added

--- src/components/ImmediateJoinerPasswordReset.js
+++ src/components/ImmediateJoinerPasswordReset.js
@@ -181,13 +181,13 @@ export class ImmediateJoinerPasswordReset extends Component {
 
                                             <div className="row form-group">
                                                 <div className="col-md-12">
-                                                    <button value="Send Me A Reset Link"
+                                                    <button 
                                                         onChange={this.handleChange}
                                                         name="IsLogIn"
                                                         onClick={event => {
                                                             this.sendResetEmail(event);
                                                         }}
-                                                        className="btn px-4 btn-primary text-white" />
+                                                        className="btn px-4 btn-primary text-white">Send Me A Reset Link</button>
                                                 </div>
                                             </div>
 
