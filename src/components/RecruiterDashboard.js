2b5764dcaf1eab7d4528d9a8eaa736b0e3c7b47f
krishnavamsi <krishnavamsideepthi@gmail.com>
2020-09-24 12:04:05 +0530

    ui fixes

--- src/components/RecruiterDashboard.js
+++ src/components/RecruiterDashboard.js
@@ -113,24 +113,25 @@ export class RecruiterDashboard extends Component {
                 <header className="site-navbar mt-3">
                     <div className="container-fluid">
                         <div className="row align-items-center">
-                            <div className="site-logo col-6"><Link to="/">Immediate Joiner</Link></div>
+                            <div className="site-logo col-6"><Link className="js-menu-toggle" to="/">Immediate Joiner</Link></div>
 
                             <nav className="mx-auto site-navigation">
                                 <ul className="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-                                    <li><Link className="nav-link js-menu-toggle" to="/">Home</Link></li>
-                                    <li><Link className="js-menu-toggle" to="/About">About</Link></li>
+                                    <li><Link className="nav-link active js-menu-toggle" to="/">Home</Link></li>
                                     <li><Link className="js-menu-toggle" to="/RecruiterDashboard">Dashboard</Link></li>
-                                    <li><Link className="js-menu-toggle" to="/Contactus">Contact</Link></li>
                                     <li class="d-lg-none"><Link className="js-menu-toggle" to="/Postjob"><span class="mr-2">+</span> Post a Job</Link></li>
-                                    <li className="d-lg-none"><button onClick={() => { auth.signOut() }} className="btn btn-primary border-width-2 d-none d-lg-inline-block js-menu-toggle">Log Out</button></li>
+                                    <li className="d-lg-none"><Link onClick={() => { auth.signOut() }} className="js-menu-toggle" >Log Out</Link></li>
                                 </ul>
                             </nav>
+
                             <div className="right-cta-menu text-right d-flex aligin-items-center col-6">
                                 <div className="ml-auto">
                                     <Link to="/Postjob" className="btn btn-outline-white border-width-2 d-none d-lg-inline-block"><span className="mr-2 icon-add"></span>Post a Job</Link>
                                     <Link onClick={() => { auth.signOut() }} className="btn btn-primary border-width-2 d-none d-lg-inline-block"><span className="mr-2 icon-lock_outline"></span>Log Out</Link>
                                 </div>
-                                <Link to="/" className="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span className="icon-menu h3 m-0 p-0 mt-2"></span></Link>
+                                <div to="/" className="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3">
+                                    <span className="icon-menu h3 m-0 p-0 mt-2"></span>
+                                </div>
                             </div>
                         </div>
                     </div>
