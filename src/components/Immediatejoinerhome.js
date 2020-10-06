766caae162196bcfbb264d52374ed0cab6a0a2c0
vamsigoteti7 <63846292+vamsigoteti7@users.noreply.github.com>
2020-10-06 08:26:30 +0000

    functiopn added

--- src/components/Immediatejoinerhome.js
+++ src/components/Immediatejoinerhome.js
@@ -172,7 +172,7 @@ class Immediatejoinerhome extends React.Component {
                                         <div className="row align-items-center justify-content-center">
                                             <div className="col-md-12">
                                                 <div className="mb-5 text-center">
-                                                    <h1 className="text-white font-weight-bold">India's First Low Priced Job Portal</h1>
+                                                    <h1 className="text-white font-weight-bold">America's First Low Priced Job Portal</h1>
                                                     <p>Spend 100 Rs To Post A Job</p>
                                                 </div>
                                             </div>
